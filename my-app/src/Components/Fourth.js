0df7fad77c309b760fe19d3de8cb74fb61c94d77
TLRKiliann <philogenie@protonmail.com>
2022-10-24 23:38:38 +0200

    some test without CRA

---
+++ my-app/src/Components/Fourth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fourth = (props) => {
+	return(
+		<div>
+			Hello 4 : id: {props.id} name: {props.name}
+		</div>
+	)
+}
+
+export default Fourth;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-class App extends React.Component{
-    render(){
-        return(
-            <div>Hello World</div>
+import ReactDOM from 'react-dom/client';
+import SecondComponent from './Components/SecondComponent';
+import Third from './Components/Third';
+import Fourth from './Components/Fourth';
+
+
+class App extends React.Component {
+    state = {data: [
+                {id: 1, name: "Paul"},
+                {id: 2, name: "Jeffrey"}
+        ]
+    };
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                {this.state.data.map(data => (
+                    <SecondComponent key={data.id} id={data.id} name={data.name}>
+                        <Third id={data.id} />
+                        <Fourth id={data.id} />
+                    </SecondComponent>
+                ))}
+            </div>
         )
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+//ReactDOM.render(<App />, document.getElementById('app'))
+const root = ReactDOM.createRoot(document.getElementById('app'));
+const element = <App />;
+root.render(element);
+
