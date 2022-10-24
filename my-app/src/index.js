9109dbac435434482c11c5bb033fffcb396a0cc7
TLRKiliann <philogenie@protonmail.com>
2022-10-24 22:18:52 +0200

    setup done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SecondComponent from './Components/SecondComponent';
-import './styles.css';
-
-class App extends React.Component {
-  render() {
-    return(
-      <div>
-        <SecondComponent />
-      </div>
-    )
-  }
+class App extends React.Component{
+    render(){
+        return(
+            <div>Hello World</div>
+        )
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
